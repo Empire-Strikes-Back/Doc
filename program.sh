3a7cf55ad4048e78b1f947d106f77ac50f63407a
Doc <Doc@Lone-Pine-Mall>
2022-07-15 08:36:50 +0300

    i revert "i revert "i remove commit count from release jar name - my release is defined entirely by commmit hash""
    This reverts commit 7a727222c43b17ce8527ff91746d7c45ae330577.

--- program.sh
+++ program.sh
@@ -14,24 +14,13 @@ main(){
     -M -m Doc.main
 }
 
-tag(){
-  COMMIT_HASH=$(git rev-parse --short HEAD)
-  COMMIT_COUNT=$(git rev-list --count HEAD)
-  TAG="$COMMIT_COUNT-$COMMIT_HASH"
-  git tag $TAG $COMMIT_HASH
-  echo $COMMIT_HASH
-  echo $TAG
-}
-
 jar(){
 
   rm -rf out/*.jar out/classes
-  COMMIT_HASH=$(git rev-parse --short HEAD)
-  COMMIT_COUNT=$(git rev-list --count HEAD)
   clojure \
     -X:Genie Genie.core/process \
     :main-ns Doc.main \
-    :filename "\"out/Doc-$COMMIT_COUNT-$COMMIT_HASH.jar\"" \
+    :filename "\"out/Doc-$(git rev-parse --short HEAD).jar\"" \
     :paths '["src"]'
 }
 
