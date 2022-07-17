a6c49278c89a1f048d3829be8f4570dcfe138d4c
Doc <Doc@Lone-Pine-Mall>
2022-07-17 19:56:22 +0300

    our 7th final element is not Leeloo - it is water - we can savour and cornometer at the corner of Jesus's table - we are that guy :Zach-Galifiankis tell us about your new movie - Furry :Brad-Pitt it's Fury

--- src/Doc/main.clj
+++ src/Doc/main.clj
@@ -20,6 +20,7 @@
    [Doc.B12]
    [Doc.salt]
    [Doc.bread]
+   [Doc.water]
    [Doc.raisins])
   (:import
    (javax.swing JFrame WindowConstants JPanel JScrollPane JTextArea BoxLayout JEditorPane ScrollPaneConstants SwingUtilities JDialog)
@@ -71,6 +72,7 @@
    '[Doc.B12]
    '[Doc.salt]
    '[Doc.bread]
+   '[Doc.water]
    '[Doc.raisins]
    '[Doc.main]
    :reload))
@@ -198,6 +200,7 @@
             jpanel-B12 (JPanel.)
             jpanel-salt (JPanel.)
             jpanel-bread (JPanel.)
+            jpanel-water (JPanel.)
             jpanel-raisins (JPanel.)]
 
         (doto jtabbed-pane
@@ -207,6 +210,7 @@
           (.addTab "B12" jpanel-B12)
           (.addTab "salt" jpanel-salt)
           (.addTab "bread" jpanel-bread)
+          (.addTab "water" jpanel-water)
           (.addTab "raisins" jpanel-raisins)
           (.setSelectedComponent jpanel-bread))
 
